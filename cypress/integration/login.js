9956c42d831ced63de80faac296fd0d17651d8d6
VissaniF <francovissani17@gmail.com>
2022-09-02 08:53:55 -0300

    Agregando visit

---
+++ cypress/integration/login.js
@@ -0,0 +1,8 @@
+describe('Login', () =>){
+    beforeEach(()=> {
+        cy.visit('http://automationpractice.com/index.php?controller=authentication&back=my-account')
+    })
+    it('Login with incorrect email', () => {
+        
+    });
+}
